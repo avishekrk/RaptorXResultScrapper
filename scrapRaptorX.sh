daf470b7ff1bcedde793c684c7e86acaa408acf0
Avishek Kumar <avishek.kumar@asu.edu>
2016-09-19 14:08:03 -0700

    scrapper working

--- scrapRaptorX.sh
+++ scrapRaptorX.sh
@@ -48,6 +48,22 @@ start_no=$1
 end_no=$2
 
 
+base_url="raptorx.uchicago.edu/ContactMap/myjobs/"
+
+begin_no=203625 #start id
+end_no=203641   #end id
+
+current_no=$begin_no
+
+while [ $current_no -le $end_no ]
+do
+    echo $current_no
+    current_no=$(( $current_no + 1))
+    url=${base_url}${current_no}summary_data/detail
+    echo $url
+    wget -O ${current_no}.zip $url 
+done
+
 
 
 
